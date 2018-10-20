6eb7f6ded00626a8711fb85ebd503fd105824d49
yekai <gaoye@tokenpai.com>
2018-10-20 10:45:09 +0800

    login

--- routes/route.go
+++ routes/route.go
@@ -67,6 +67,48 @@ func Register(c echo.Context) error {
 	return nil
 }
 
+//登陆功能
+func Login(c echo.Context) error {
+	//1. 响应数据结构初始化
+	var resp utils.Resp
+	resp.Errno = utils.RECODE_OK
+	defer utils.ResponseData(c, &resp)
+
+	//2. 解析数据
+	account := &dbs.Account{}
+	if err := c.Bind(account); err != nil {
+		fmt.Println(account)
+		resp.Errno = utils.RECODE_PARAMERR
+		return err
+	}
+
+	//3. 操作mysql-查询数据
+	//sql:select * from account where username ='yekai' and identity_id='yekai';
+	sql := fmt.Sprintf("select * from account where username ='%s' and identity_id='%s'",
+		account.UserName,
+		account.IdentityID,
+	)
+	fmt.Println(sql)
+	m, n, err := dbs.DBQuery(sql)
+	if err != nil || n <= 0 {
+		fmt.Println("failed to query account", err)
+		resp.Errno = utils.RECODE_DBERR
+		return err
+	}
+	//m 是一个[]map[string]string
+	rows := m[0]
+	//4. session处理
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 7,
+		HttpOnly: true,
+	}
+	sess.Values["address"] = rows["address"]
+	sess.Save(c.Request(), c.Response())
+	return nil
+}
+
 //session获取
 func GetSession(c echo.Context) error {
 	var resp utils.Resp
